709c5b0aba5c8a5a6390db2611806d1cc2c7cd09
Burdzi0 <burdzi0@programmer.net>
2019-05-08 10:19:07 +0200

    Schedulded task modification

--- src/main/java/org/burdzi0/bootshorten/service/ScheduldedRemovingLinksTask.java
+++ src/main/java/org/burdzi0/bootshorten/service/ScheduldedRemovingLinksTask.java
@@ -4,8 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
 
 @Component
 public class ScheduldedRemovingLinksTask {
@@ -18,9 +16,8 @@ public class ScheduldedRemovingLinksTask {
 	}
 
 	@Scheduled(cron = "* * * * *")
-	public void deleteOldLinks() {
+	public void deleteOldLinks() { //TODO Test delete old links schedulded task
 		int deleted = linkService.deleteAllOldLinks();
 		logger.info("Deleted :" + deleted + " old links");
-		System.out.println("ABC");
 	}
 }
