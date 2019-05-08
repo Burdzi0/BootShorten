ff47f7e6c5274b252bec3dc1fc215b3899e4e46c
Burdzi0 <burdzi0@programmer.net>
2019-05-08 11:37:49 +0200

    Change CrudRepository to JpaRepository

--- src/main/java/org/burdzi0/bootshorten/repository/LinkRepository.java
+++ src/main/java/org/burdzi0/bootshorten/repository/LinkRepository.java
@@ -1,10 +1,10 @@
 package org.burdzi0.bootshorten.repository;
 
 import org.burdzi0.bootshorten.model.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
-public interface LinkRepository extends CrudRepository<Link, Long>{
+public interface LinkRepository extends JpaRepository<Link, Long> {
 	@Query("DELETE FROM Link l WHERE l.expiration < CURRENT_TIMESTAMP")
 	int removeOldLinks();
 }
