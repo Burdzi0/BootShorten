6223241e66a66bd5e726866a64b235dec5f000eb
Burdzi0 <burdzi0@programmer.net>
2019-06-12 00:22:42 +0200

    Fix scheduling

--- src/main/java/org/burdzi0/bootshorten/repository/LinkRepository.java
+++ src/main/java/org/burdzi0/bootshorten/repository/LinkRepository.java
@@ -2,9 +2,12 @@ package org.burdzi0.bootshorten.repository;
 
 import org.burdzi0.bootshorten.model.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface LinkRepository extends JpaRepository<Link, Long> {
+
+	@Modifying
 	@Query("DELETE FROM Link l WHERE l.expiration < CURRENT_TIMESTAMP")
 	int removeOldLinks();
 }
--- src/main/java/org/burdzi0/bootshorten/service/ScheduledRemovingLinksTask.java
+++ src/main/java/org/burdzi0/bootshorten/service/ScheduledRemovingLinksTask.java
@@ -5,18 +5,21 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
+
 @Component
-public class ScheduldedRemovingLinksTask {
+public class ScheduledRemovingLinksTask {
 
 	private LinkService linkService;
 	private Logger logger = LogManager.getLogger();
 
-	public ScheduldedRemovingLinksTask(LinkService linkService) {
+	public ScheduledRemovingLinksTask(LinkService linkService) {
 		this.linkService = linkService;
 	}
 
-	@Scheduled(cron = "0 * * * * *")
-	public void deleteOldLinks() { //TODO Test delete old links schedulded task
+	@Scheduled(cron = "0 0/30 * ? * *")
+	@Transactional
+	public void deleteOldLinks() {
 		int deleted = linkService.deleteAllOldLinks();
 		logger.info("Deleted :" + deleted + " old links");
 	}
